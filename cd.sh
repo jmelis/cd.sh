184c74a42d90087c46407facc4db8653a96a8795
Jaime Melis <j.melis@fdi.ucm.es>
2011-08-01 12:43:48 +0200

    Initial commit

---
+++ cd.sh
@@ -0,0 +1,24 @@
+TO_DIR=$HOME/.to
+TO_DB=$TO_DIR/db
+
+mkdir -p $TO_DIR
+
+function cd_bookmark () {
+    echo "$1:$PWD" >> $TO_DB
+}
+
+function cd() {
+    if [ -d "$1" -o "$1" = "-" ]; then
+        builtin cd "$1"
+    elif [ -z "$1" ]; then
+        builtin cd
+    else
+        dir=$(grep ^$1: $TO_DB|cut -d: -f2)
+        if [ -n "$dir" ]; then
+            echo "$dir"
+            cd "$dir"
+        else
+            builtin cd "$1"
+        fi
+    fi
+}
