3b812fcd60d7b8d2642c92ecb1275ae45cf0cbbb
Jaime Melis <j.melis@fdi.ucm.es>
2011-09-05 12:09:25 +0200

    add installation script

---
+++ install.sh
@@ -0,0 +1,16 @@
+if [ -d ~/.cd ]; then
+    echo -e "\033[0;33mYou already have cd.sh installed.\033[0m"
+    exit
+fi
+
+echo -e "\033[0;34mCloning cd.sh...\033[0m"
+/usr/bin/env git clone https://github.com/jmelis/cd.sh ~/.cd
+
+echo -e "\033[0;33mBacking up to ~/.bashrc.pre-cd.sh\033[0m";
+cp ~/.bashrc ~/.bashrc.pre-cd.sh;
+
+echo -e  "\033[0;34mSource cd.sh with bash.\033[0m"
+echo -e "\nsource ~/.cd/cd.sh" >> ~/.bashrc
+
+echo -e "\n\n \033[0;32mcd.sh is now installed.\033[0m"
+source ~/.cd/cd.sh
