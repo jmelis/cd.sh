abca1be4d3348d7046e8d8a1ada3fa39d5b2dea9
Jaime Melis <jmelis@opennebula.org>
2014-12-22 10:59:11 +0100

    update

--- cd.sh
+++ cd.sh
@@ -3,17 +3,22 @@ TO_DB=$TO_DIR/db
 
 mkdir -p $TO_DIR
 
+function get_dir() {
+    grep "^${1}:" $TO_DB|cut -d: -f2-
+}
+
 function cd_bookmark () {
     echo "$1:$PWD" >> $TO_DB
 }
 
 function cd() {
+    local dir
     if [ -d "$1" -o "$1" = "-" ]; then
         builtin cd "$1"
     elif [ -z "$1" ]; then
         builtin cd
     else
-        dir=$(grep ^$1: $TO_DB|cut -d: -f2-)
+        dir=$(get_dir $1)
         if [ -n "$dir" ]; then
             echo "$dir"
             cd "$dir"
--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 if [ -d ~/.cd ]; then
-    echo -e "\033[0;33mYou already have cd.sh installed.\033[0m"
+    echo -e "You already have cd.sh installed."
     exit
 fi
 
-echo -e "\033[0;34mCloning cd.sh...\033[0m"
-/usr/bin/env git clone https://github.com/jmelis/cd.sh ~/.cd
+echo -e "Cloning cd.sh..."
+OUT=$(/usr/bin/env git clone -q https://github.com/jmelis/cd.sh ~/.cd 2>&1)
 
-echo -e "\033[0;33mBacking up to ~/.bashrc.pre-cd.sh\033[0m";
+if [ "$?" != "0" ]; then
+    echo "$OUT"
+fi
+
+echo -e "Backing up to ~/.bashrc.pre-cd.sh";
 cp ~/.bashrc ~/.bashrc.pre-cd.sh;
 
-echo -e  "\033[0;34mSource cd.sh with bash.\033[0m"
 echo -e "\nsource ~/.cd/cd.sh" >> ~/.bashrc
-
-echo -e "\n\n \033[0;32mcd.sh is now installed.\033[0m"
+echo ""
+echo -e "cd.sh is now installed."
 echo -e "Open a new terminal or run '\033[0;32msource ~/.cd/cd.sh\033[0m'"
